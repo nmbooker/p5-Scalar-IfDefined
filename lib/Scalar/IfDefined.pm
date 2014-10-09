158988f1ff86c8ee7e97b6b48ff41f2168c6826f
Nick-OpusVL <Nick-OpusVL@users.noreply.github.com>
2014-10-09 14:52:01 +0100

    Updated perldoc.

--- lib/Scalar/IfDefined.pm
+++ lib/Scalar/IfDefined.pm
@@ -52,8 +52,9 @@ our $VERSION = '0.01';
 
 =head1 EXPORT
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=over 4
+=item ifdef
+=back
 
 =head1 SUBROUTINES/METHODS
 
@@ -74,12 +75,6 @@ sub ifdef(&$) {
     return defined($scalar) ? $block->($scalar) : $scalar;
 }
 
-=head2 function2
-
-=cut
-
-sub function2 {
-}
 
 =head1 AUTHOR
 
@@ -87,41 +82,7 @@ Nick Booker, C<< <NMBooker at gmail.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-scalar-ifdefined at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Scalar-IfDefined>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc Scalar::IfDefined
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Scalar-IfDefined>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Scalar-IfDefined>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Scalar-IfDefined>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Scalar-IfDefined/>
-
-=back
+L<https://github.com/nmbooker/p5-Scalar-IfDefined/issues>
 
 
 =head1 ACKNOWLEDGEMENTS
