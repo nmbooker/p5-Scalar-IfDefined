9772d4187a7646b62e28289cdf6da3f39b8e7878
Nick Booker <NMBooker@gmail.com>
2014-10-09 08:59:14 +0100

    Initial commit - generated with module-starter

---
+++ lib/Scalar/IfDefined.pm
@@ -0,0 +1,111 @@
+package Scalar::IfDefined;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Scalar::IfDefined - The great new Scalar::IfDefined!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Scalar::IfDefined;
+
+    my $foo = Scalar::IfDefined->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Nick Booker, C<< <NMBooker at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-scalar-ifdefined at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Scalar-IfDefined>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Scalar::IfDefined
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Scalar-IfDefined>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Scalar-IfDefined>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Scalar-IfDefined>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Scalar-IfDefined/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2014 Nick Booker.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Scalar::IfDefined
