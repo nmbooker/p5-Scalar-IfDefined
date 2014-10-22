d4088b2644344a950349ca0f9fb0e0668ac84ad4
Nick Booker <nick.booker@opusvl.com>
2014-10-22 15:48:42 +0100

    Bumped version number

--- lib/Scalar/IfDefined.pm
+++ lib/Scalar/IfDefined.pm
@@ -13,11 +13,11 @@ Scalar::IfDefined - Apply block to scalar depending on if it's defined.
 
 =head1 VERSION
 
-Version 0.02
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
